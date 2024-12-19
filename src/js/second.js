ff74dccd08ffecad3b0597d427ad58fc30768882
kmillee <jouanet.camille@gmail.com>
2024-12-19 23:57:01 +0100

    automatic grid w/ charts (bars only)

--- src/js/second.js
+++ src/js/second.js
@@ -12,6 +12,9 @@ const ctx = {
 
 };
 
+
+
+
 const dataJson = {"1" : "data/QB12_2.json",
                 "2" : "data/QB13_2.json",
                 "3":"data/QB13_3.json"
@@ -70,127 +73,70 @@ async function loadDataJson(questID) {
 }
 
 function addChart() {
-    if (ctx.CHART_NB === 10){
+    if (ctx.CHART_NB === 12){
         console.log("Too many charts, please make some room before continuing.");
     }
     else{
         const chartType = document.getElementById("chartType").value; 
         const selectedQuestion = document.getElementById("questionList").value;
+
         loadDataJson(selectedQuestion).then(selectedData => {
-            ctx.CHART_NB++;  
-            createChart(chartType, selectedData);
+            ctx.CHART_NB++;
+            // Example of using sample data for a bar chart
+            const sampleData = [
+                { category: "A", value: 30 },
+                { category: "B", value: 80 },
+                { category: "C", value: 45 },
+                { category: "D", value: 60 },
+                { category: "E", value: 20 },
+                { category: "F", value: 90 },
+                { category: "G", value: 55 }
+            ];
+
+            createChart(chartType, sampleData);
+            // createChart(chartType, selectedData);
             rearrangeChart();
         }); 
     }
 }
 
-// function createChart(chartType, data) {
-
-//     console.log(`Creating chart #${ctx.CHART_NB} of type ${chartType} with data:`, data);
-
-//     const numCols = Math.ceil(Math.sqrt(ctx.CHART_NB)); 
-//     const numRows = Math.ceil(ctx.CHART_NB / numCols); 
-
-//     const width = ctx.SVG_W / numCols; 
-//     const height = ctx.SVG_H / numRows
-
-//     const svg = d3.select("svg");
-
-//     //select every existing rectangular + new one, update their positions
-//     svg.selectAll("rect")  
-//     .data(d3.range(ctx.CHART_NB))  // going through all rects
-//     .join(
-//         enter => {
-//             const newRect = enter.append("rect")
-//                 .attr("width", width)
-//                 .attr("height", height)
-//                 .attr("fill", "steelblue")
-//                 .attr("x", (d, i) => (i % numCols) * width) 
-//                 .attr("y", (d, i) => Math.floor(i / numCols) * height)
-//                 .attr("rx", 10)
-//                 .attr("ry", 10);
-            
-//             return newRect;
-//         },
-//         update => {
-//             update.transition() 
-//                 .attr("width", width)
-//                 .attr("height", height)
-//                 .attr("x", (d, i) => (i % numCols) * width) 
-//                 .attr("y", (d, i) => Math.floor(i / numCols) * height); 
-
-//             return update;
-//         }
-//     );
-
-
-//     // if (chartType === "bar") {
-//     //     console.log("Bar chart");
-
-//     //     const shape = svg.append("rect")  // Créer un carré
-//     //     .attr("x", width)  // Position X en fonction du nombre de graphiques
-//     //     .attr("y", height)  // Position Y fixe pour l'instant (on peut aussi l'adapter)
-//     //     .attr("width", 50)  // Largeur du carré
-//     //     .attr("height", 50)  // Hauteur du carré
-//     //     .attr("fill", "steelblue");  // Couleur de remplissage
-
-//     //     // const x = d3.scaleBand().domain(data.map(d => d.id)).range([0, 400]).padding(0.1);
-//     //     // const y = d3.scaleLinear().domain([0, d3.max(data, d => d.number_of_respondents)]).nice().range([300, 0]);
-
-//     //     // svg.selectAll(".bar")
-//     //     //     .data(data)
-//     //     //     .enter().append("rect")
-//     //     //     .attr("x", d => x(d.id))
-//     //     //     .attr("y", d => y(d.number_of_respondents))
-//     //     //     .attr("width", x.bandwidth())
-//     //     //     .attr("height", d => 300 - y(d.number_of_respondents))
-//     //     //     .attr("fill", "steelblue");
-        
-//     //     // svg.append("g")
-//     //     //     .attr("transform", "translate(0,300)")
-//     //     //     .call(d3.axisBottom(x));
-
-//     //     // svg.append("g")
-//     //     //     .call(d3.axisLeft(y));
-//     // }
-//     // else if (chartType === "line"){
-//     //     console.log("Line chart");
-
-//     //     const shape = svg.append("rect")  // Créer un carré
-//     //     .attr("x", (ctx.CHART_NB - 1) * width)  // Position X en fonction du nombre de graphiques
-//     //     .attr("y", 0)  // Position Y fixe pour l'instant (on peut aussi l'adapter)
-//     //     .attr("width", width)  // Largeur du carré
-//     //     .attr("height", height)  // Hauteur du carré
-//     //     .attr("fill", "green");  // Couleur de remplissage
-//     // }
-//     // else if (chartType === "scatter"){
-//     //     console.log("Scatter plot");
-
-//     //     const shape = svg.append("rect")  // Créer un carré
-//     //     .attr("x", (ctx.CHART_NB - 1) * width)  // Position X en fonction du nombre de graphiques
-//     //     .attr("y", 0)  // Position Y fixe pour l'instant (on peut aussi l'adapter)
-//     //     .attr("width", width)  // Largeur du carré
-//     //     .attr("height", height)  // Hauteur du carré
-//     //     .attr("fill", "red");  // Couleur de remplissage
-//     // }
-
-//     // Ajouter d'autres types de graphiques selon le chartType...
-// }
 
 function createChart(chartType, data) {
     const svg = d3.select("svg");
 
-    svg.append("rect")
-        .attr("class", `chart-${ctx.CHART_NB}`)  //keep an id
-        .attr("data-type", chartType)          
-        .attr("width", 0)                     
-        .attr("height", 0)
-        .attr("x", ctx.SVG_W / 2)  
-        .attr("y", ctx.SVG_H / 2)
-        .attr("fill", chartType === "bar" ? "steelblue" : chartType === "line" ? "orange" : "green")  // Couleurs différentes
-        .attr("rx", 10)      
-        .attr("ry", 10)
-        .style("opacity",0)
+    const numCols = Math.ceil(Math.sqrt(ctx.CHART_NB));
+    const numRows = Math.ceil(ctx.CHART_NB / numCols);
+
+    const width = ctx.SVG_W / numCols;
+    const height = ctx.SVG_H / numRows;
+
+    const x = ((ctx.CHART_NB - 1) % numCols) * width;
+    const y = Math.floor((ctx.CHART_NB - 1) / numCols) * height;
+///////////
+    if (chartType === "bar") {
+        console.log("bar chart")
+        addBarPlot(svg, x, y, width, height, data,ctx.CHART_NB);
+    }
+    // if (chartType === "line") {
+    //     console.log("line chart")
+    //     addLinePlot(svg, x, y, width, height, data,ctx.CHART_NB);
+    // }
+    else{
+        console.log("other chart")
+        svg.append("rect")
+            .attr("class", `chart-${ctx.CHART_NB}`)  //keep an id
+            .attr("data-type", chartType)          
+            .attr("width", 0)                     
+            .attr("height", 0)
+            .attr("x", ctx.SVG_W / 2)  
+            .attr("y", ctx.SVG_H / 2)
+            .attr("fill", chartType === "bar" ? "steelblue" : chartType === "line" ? "orange" : "green")  // Couleurs différentes
+            .attr("rx", 10)      
+            .attr("ry", 10)
+            .style("opacity",0)
+    }
+
+    
 }
 
 function rearrangeChart() {
@@ -202,19 +148,110 @@ function rearrangeChart() {
     const width = ctx.SVG_W / numCols;
     const height = ctx.SVG_H / numRows;
 
-    svg.selectAll("rect")
-        .data(d3.range(ctx.CHART_NB))  //each rect has its index
+
+    svg.selectAll(".chart")
+        .data(d3.range(ctx.CHART_NB))
         .transition()  
         .duration(500)
-        .attr("width", width)
-        .attr("height", height)
-        .attr("x", (d, i) => (i % numCols) * width)  
-        .attr("y", (d, i) => Math.floor(i / numCols) * height)
-        .style("opacity",1)
+        .attr("transform", (d, i) => {
+            const col = i % numCols;
+            const row = Math.floor(i / numCols);
+            return `translate(${col * width}, ${row * height})`; 
+        })
+        // We have to recompute scales and margins every time
+        .each(function() {
+            const g = d3.select(this);
+
+            // Margins + dimensions
+            const margin = { top: 10, right: 10, bottom: 50, left: 10 };
+            const plotWidth = width - margin.left - margin.right;
+            const plotHeight = height - margin.top - margin.bottom;
+
+            // Scales
+            const xScale = d3.scaleBand()
+                .domain(g.selectAll(".bar").data().map(d => d.category))
+                .range([0, plotWidth])
+                .padding(0.1);
+
+            const yScale = d3.scaleLinear()
+                .domain([0, d3.max(g.selectAll(".bar").data(), d => d.value)])
+                .nice()
+                .range([plotHeight, 0]);
+
+            // Redraw axis
+            g.select(".x-axis")
+                .transition()
+                .duration(500)
+                .attr("transform", `translate(${margin.left},${margin.top + plotHeight})`)
+                .call(d3.axisBottom(xScale).tickSizeOuter(0));
+
+            g.select(".y-axis")
+                .transition()
+                .duration(500)
+                .attr("transform", `translate(${margin.left},${margin.top})`)
+                .call(d3.axisLeft(yScale).ticks(5));
+
+            // update dimensions
+            g.selectAll(".bar")
+                .transition()
+                .duration(500)
+                .attr("x", d => xScale(d.category) + margin.left)
+                .attr("y", d => yScale(d.value) + margin.top)
+                .attr("width", xScale.bandwidth())
+                .attr("height", d => plotHeight - yScale(d.value))
+                .style("opacity", 1)
+        });
 }
 
 
-function addBarPlot(data){}
+function addBarPlot(svg, x, y, width, height, data,chartId) {
+    const margin = { top: 10, right: 10, bottom: 30, left: 40 };
+    const plotWidth = width - margin.left - margin.right;
+    const plotHeight = height - margin.top - margin.bottom;
+
+    const g = svg.append("g")
+        .attr("transform", `translate(${x},${y})`)
+        .attr("class", `chart ${chartId}`);    //to move the container
+
+    const chartArea = g.append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // Scales
+    const xScale = d3.scaleBand()
+        .domain(data.map(d => d.category))
+        .range([0, plotWidth])
+        .padding(0.1);
+
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.value)])
+        .nice()
+        .range([plotHeight, 0]);
+
+    // Axes
+    chartArea.append("g")
+        .attr("class", "axis x-axis")
+        .attr("transform", `translate(0,${plotHeight})`)
+        .call(d3.axisBottom(xScale).tickSizeOuter(0));
+
+    chartArea.append("g")
+        .attr("class", "axis y-axis") 
+        .call(d3.axisLeft(yScale).ticks(5));
+
+    // Bars
+    chartArea.selectAll(".bar")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", d => xScale(d.category))
+        .attr("y", d => yScale(d.value))
+        .attr("width", xScale.bandwidth())
+        .attr("height", d => plotHeight - yScale(d.value))
+        .attr("fill", "steelblue")
+        .style("opacity", 0)
+
+}
+
 
 function addLinePlot(data){}
 
